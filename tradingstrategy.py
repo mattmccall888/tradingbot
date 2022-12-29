18f049f19364f122fcbbb5cb7ea5252d3fa13f19
Matt McCall <mattbear888@gmail.com>
2022-12-28 21:50:47 -0600

    feat: start of trader

--- tradingstrategy.py
+++ tradingstrategy.py
@@ -3,8 +3,16 @@ import robin_stocks as rs
 import robin_stocks.helper as helper
 import robin_stocks.urls as url
 
-class debit_trader():
-    def __init__(self, stocks):
-        self.stocks = stocks
+def spreadcriteriacheck(stocks):
+    goodstocks = []
+    stocks = stocks
+    for i in stocks:
+        
+        
+
+
+
+
+
 
 

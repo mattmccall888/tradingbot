17e41f4943b8ca3e65eb1ad4609616888770691b
Matt McCall <mattbear888@gmail.com>
2022-12-28 21:17:19 -0600

    feat: trading strategy section

---
+++ tradingstrategy.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import robin_stocks as rs
+import robin_stocks.helper as helper
+import robin_stocks.urls as url
+
+class debit_trader():
+    def __init__(self, stocks):
+        self.stocks = stocks
+
+
